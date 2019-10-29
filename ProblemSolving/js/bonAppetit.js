e279b532387111bfa7b1e16fd315b637d63d90a4
LisaCee <lisaceedesign@gmail.com>
2019-10-28 19:41:00 -0700

    bon appetit

---
+++ ProblemSolving/js/bonAppetit.js
@@ -0,0 +1,47 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the bonAppetit function below.
+function bonAppetit(bill, k, b) {
+    let total = bill.reduce((a, b) => a + b)
+    // all items
+    if ((total - bill[k])/2 === b) {
+        // all items - k
+        console.log('Bon Appetit')
+    } else {
+        console.log( b - (total - bill[k]) /2)
+    }
+}
+
+function main() {
+    const nk = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const bill = readLine().replace(/\s+$/g, '').split(' ').map(billTemp => parseInt(billTemp, 10));
+
+    const b = parseInt(readLine().trim(), 10);
+
+    bonAppetit(bill, k, b);
+}
