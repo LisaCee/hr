f609301b3b354e27ce5f2d96a5bd6c1386057705
LisaCee <lisaceedesign@gmail.com>
2019-11-26 18:36:54 -0800

    day of programmer psuedocode

--- ProblemSolving/js/dayOfProgrammer.js
+++ ProblemSolving/js/dayOfProgrammer.js
@@ -1,36 +1,15 @@
 function dayOfProgrammer(year) {
     //day of programmer = 256
-    let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    let days = 365
-    if (year >= 1700 && year <= 1917) {
-      console.log('Julian')
-      //leap years % 4 === 0
-      if (year % 4 === 0) {
-        months[1] = 29;
-        days += 1
-      }
+    // julian 1700 - 1917
+    // leap / 4
+    // gregorial 1919-2700
+    // leap / 400 or leap / 4 !100
+    // 1918, January 31 -> Feb 14 (32nd day)
   
-    for (let i = months.length -1; i > 0; i--) {
-  
-      while (days > 256) {
-        days -= months[i]
-        console.log(days)
-      }
-    }
-    }
-    // } else if (year >= 1919 && year <= 2700) {
-    //   console.log('Gregorian')
-    //   // leap years % 400 === 0 or leap years % 4 !100
-    //   if (year % 400 === 0) {
-  
-    //   } else if (year % 4 === 0 && year % 100 !== 0) {
-  
-    //   }
-    // } else if (year === 1918) {
-    //   console.log('Transition year')
-    // }
-    // return dd/mm/yyyy
-  
+
+    // figure out calendar system
+    // leap year
+    // end of year - 109 days
   }
   
   dayOfProgrammer(1916)
